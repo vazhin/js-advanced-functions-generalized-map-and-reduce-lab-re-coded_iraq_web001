b9990ba845fd13fcf3007007660f3bdaeedcbf81
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 11:35:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function map(arr, callbackFn) {
 
 function reduce(arr, callbackFn, startingPoint) {
   let newValue = startingPoint ? startingPoint : arr[0]
-
   for (let i = startingPoint ? 0 : 1; i < arr.length; i++) {
     newValue = callbackFn(arr[i], newValue)
   }
