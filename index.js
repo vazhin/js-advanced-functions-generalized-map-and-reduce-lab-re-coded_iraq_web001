6f7eca6ba07da2ed2495577d712296ed82473610
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 11:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, callbackFn) {
+  let newArr = []
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(callbackFn(arr[i]))
+  }
+  return newArr
+}
+
+function reduce(arr, callbackFn, startingPoint) {
+  let newValue;
+  if (startingPoint){
+    newValue = startingPoint;
+  }
+  for (var i = 0; i < arr.length; i++) {
+    newValue = callbackFn(arr[i], newValue)
+  }
+  return newValue
+}
